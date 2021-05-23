08216723a44bace8db20956cb43393bd26bac46f
myeongjooseo <juux_x@naver.com>
2021-05-23 20:25:19 +0900

    2강 : 멤버, 주문, 할인 도메인 개발

---
+++ src/test/java/hello/core/member/MemberServiceImplTest.java
@@ -0,0 +1,25 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceImplTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L, "MemberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        assertThat(member).isEqualTo(findMember);
+    }
+}
---
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,33 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        //given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        //when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        //then
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+
+
+}
