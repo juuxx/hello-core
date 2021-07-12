28e09fe7dd6d54b3e6721fcaa4e7c9afb7f89cbe
myeongjooseo <juux_x@naver.com>
2021-07-13 00:27:23 +0900

    :wrench: @ComponentScan 추가

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private  MemberRepository memberRepository;
 
     //memberserivceImpl 의 생성자
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
