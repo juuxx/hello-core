69de68b932a656741221433d974de1b26c34b0a7
myeongjooseo <juux_x@naver.com>
2021-07-14 00:33:10 +0900

    :zap: bean 등록 시 자동 등록과 수동 등록이 충돌할 떄

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -7,8 +10,12 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
         //basePackages = "hello.core.member",
-        //default = 이 클래스가 위치한 패키지 부터 스캔
+        //default = 이 클래스가 위치한 패키지 부터 스
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
