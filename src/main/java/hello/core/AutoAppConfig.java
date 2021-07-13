a8d34b7c845291e5b27ff2ac07291ccb0b0c0e0b
myeongjooseo <juux_x@naver.com>
2021-07-14 00:04:11 +0900

    :zap: filter 추가

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //basePackages = "hello.core.member",
+        //default = 이 클래스가 위치한 패키지 부터 스캔
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
